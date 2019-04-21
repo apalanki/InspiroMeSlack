05945b1af1d30482c1a0563d531292b0d21c51a1
Anudeep Palanki <palankianudeep@gmail.com>
2019-04-21 08:57:01 -0500

    remove redundant error handler

--- app.js
+++ app.js
@@ -11,12 +11,6 @@ app.get('/', function(req, res) {
     res.status(200).send('Ready to Inspire!');
 });
 
-// error handler
-app.use(function(err, req, res) {
-    console.error(err.stack);
-    res.status(400).send(err.message);
-});
-
 app.listen(port, function() {
     console.log('InspiroBot listening on port ' + port);
 });
